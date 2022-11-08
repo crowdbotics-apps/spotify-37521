13c1dd16adf202990e1201111430be977b0b0243
Crowdbotics <team@crowdbotics.com>
2022-11-08 08:33:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "spotify_37521",
+  url: "https://spotify_37521.botics.co",
+  api: "https://spotify_37521.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
